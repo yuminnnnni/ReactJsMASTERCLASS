86adc57a801ab0a6ac26710b4a85173be0fa5be7
yuminnnnni <ka09023@ajou.ac.kr>
2024-09-19 02:06:36 +0900

    Feat: styled components로 애니메이션 쿠현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotateAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  5% {
+    border-radius:100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color:tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+  ${Emoji} {
+    &:hover{
+      font-size: 100px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <div></div>
-      <div></div>
-    </div>
+    <Wrapper>
+      <Box>
+        <Emoji as='a'>🥹</Emoji>
+        <Emoji as='span'>🥹</Emoji>
+      </Box>
+      <Emoji as='a'>🥹</Emoji>
+    </Wrapper>
   )
 }
 
