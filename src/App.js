c920f8219f9c29446a8e624eef10890430371920
yuminnnnni <ka09023@ajou.ac.kr>
2024-09-19 02:45:10 +0900

    Feat: 다크 모드, 라이트 모드 구현

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
 import styled, { keyframes } from 'styled-components';
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  5% {
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color:tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover{
-      font-size: 100px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as='a'>🥹</Emoji>
-        <Emoji as='span'>🥹</Emoji>
-      </Box>
-      <Emoji as='a'>🥹</Emoji>
+      <Title>
+        Hello
+      </Title>
     </Wrapper>
   )
 }
